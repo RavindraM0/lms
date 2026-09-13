127adc88c1973353c45cff84229f4cb6736b2525
RavindraMO <ravindramo2005@gmail.com>
2026-09-13 23:09:42 +0530

    Place Hardik P between Preetham A and Ravindra M.O

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,14 +15,14 @@ import suheartImg from "@/assets/images/team/suheart.jpg";
 
 const team = [
   ["Preetham A", "Founder", preethamImg],
+  ["Hardik P", "Co Founder / Content Strategist", hardikImg],
   ["Ravindra M.O", "Web Developer", ravindraImg],
   ["Chetas", "Creative Head", chetasImg],
   ["Vishnu Reddy", "Executive Manager", vishnuImg],
-  ["Suheart", "Cinematographer", hardikImg],
+  ["Suheart", "Cinematographer", suheartImg],
   ["Gokul", "Graphic Designer", gokulImg],
   ["Sanjana A", "Finance Co-ordinator", sanjanaImg],
   ["Sagar RS", "Technical Advisor", sagarImg],
-  ["Hardik P", "Co Founder / Content Strategist", suheartImg],
 ];
 
 export default function About() {
